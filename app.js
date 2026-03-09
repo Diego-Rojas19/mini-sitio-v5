2e7c4e0ea6d506d5ae0827f83090ffaf64c112a9
diego <tekasy72@gmail.com>
2026-03-08 20:46:22 -0500

    feat: proyecto Mini-Sitio v5 inicial

---
+++ app.js
@@ -0,0 +1,8 @@
+let titulo = "Mini-Sitio ADSO"
+const saludo = "Hola " + titulo.toUpperCase()
+console.log(saludo)
+
+function mostrarMensaje(nombre) {
+  const mensaje = "Bienvenido " + nombre
+  console.log(mensaje)
+}
